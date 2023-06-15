f6be21c7d567a17c498df71c905a397115e669d5
Chehlat <107953835+Chehlat@users.noreply.github.com>
2023-06-15 22:33:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var userName = prompt("Введите ваше имя:");
+
+if (userName === "Никита" || userName === "Никитос" || userName === "Ник") 
+{
+    alert("Привет, хозяин!");
+} 
+
+else if (userName === "Вася" || userName === "Петя" || userName === "Маша"); 
+{
+    alert("Привет, дружище!");
+} 
+else 
+{
+  alert("Привет!");
+}
